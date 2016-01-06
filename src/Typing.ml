7263fa969a2a46a3bfed2c7ce515039f34e66472
raphaelrieuhelft <raphael.rieuhelft@gmail.com>
2016-01-06 11:08:30 +0100

    Finish implementing the typechecker

--- src/Typing.ml
+++ src/Typing.ml
@@ -11,8 +11,26 @@ let type_error msg = raise (Type_error msg)
 
 (* Subtyping check *)
 
-let rec subtype t1 t2 = false 
-
+let rec subtype t1 t2 = 
+  let rec aux l1 l2 =
+	match l2 with
+	|[] -> true
+	|(lab, t2)::l -> 
+		begin 
+			try let t1 = List.assoc lab l1 in if subtype t1 t2 then aux (List.remove_assoc lab l1) l else false
+			with Not_found -> false
+		end
+  in
+  match (t1,t2) with
+	| _,Top -> true
+	| x,y when x=y -> true
+	| Int, Float -> true
+	| Arrow(s1,s2), Arrow(t1,t2) -> (subtype t1 s1)&&(subtype s2 t2)
+	| Record l1, Record l2 -> aux l1 l2
+	|_-> false
+	
+	
+	
 (* Infer the principal type for expression [e] in typing environment [env].
    Raise [Type_error] if the expression is ill-typed. *)
 
@@ -25,7 +43,13 @@ let rec infer env e =
       | None -> type_error (sprintf "unbound variable %s" v)
       end
   | Eabstr (v, t1, e) -> let t2 = infer (add_typenv v t1 env) e in Arrow(t1,t2)
-  
+  | Eapp (e1, e2) ->
+	  begin match infer env e1 with
+	  | Arrow(t1,t2) -> check env e2 t1; t2
+	  | t -> raise (Type_error ("attempting to apply to a non-function of type "^(pretty_typ t)))
+	  end
+  | Elet (v, e1, e2) ->
+	  let t1 = infer env e1 in infer (add_typenv v t1 env) e2
   (* Arithmetic *)
   | Econst c ->
       type_of_constant c
@@ -40,21 +64,25 @@ let rec infer env e =
       tres
   (* Records *)
   | Erecord l -> 
-	begin
-	match l with 
-	|[] -> Record []
-	|(lab,e)::t -> (match infer env (Erecord t) with Record l -> Record ((lab, infer env e)::l) | _-> assert false)
-	end
+	  begin match l with 
+	  |[] -> Record []
+	  |(lab,e)::t -> 
+		begin match infer env (Erecord t) with 
+		| Record l ->
+			if List.mem_assoc lab l 
+			then raise (Type_error ("label "^lab^" appears twice in this record"))  
+			else Record ((lab, infer env e)::l) 
+		| _-> assert false
+		end
+	  end
   | Efield (e,lab) -> 
-	begin
-	match infer env e with 
-		|Record l -> (try List.assoc lab l with Not_found -> raise (Type_error ("label "^lab^" not present in this record")))
-		|t -> raise (Type_error ("attempting to type label "^lab^" of the non-record "^(pretty_typ t)))
-	end
+	  begin match infer env e with 
+	  |Record l -> (try List.assoc lab l with Not_found -> raise (Type_error ("label "^lab^" not present in this record")))
+	  |t -> raise (Type_error ("attempting to type label "^lab^" of a non-record of type "^(pretty_typ t)))
+	  end
   (* Type constraint *)
   | Econstraint(e, t) ->
       check env e t; t
-  | _ -> type_error "TODO" 
 
 (* Check that expression [e] has type [t] in typing environment [env].
    Return [()] if true.  Raise [Type_error] if not. *)
