6ee0bbc3b8bd3c6d748f387f782eda6ca3e46dd3
raphaelrieuhelft <raphael.rieuhelft@gmail.com>
2016-02-26 19:47:59 +0100

    Fix apply_coercion to work on variables, add a simple optimization

--- src/Expand.ml
+++ src/Expand.ml
@@ -14,35 +14,22 @@ let rec elt i = function
   |_ ::t -> elt (i-1) t
 
 let append_tuple (l: lam) = function Ltuple l2 -> Ltuple (l::l2) |_-> assert false 
-  
-let rec subst (v:variable) (t:lam) (l:lam) = 
-  let my_subst = subst v t in
-  match l with
-  | Lvar v1 when v=v1 -> t
-  | Lvar v1 -> Lvar v1
-  | Labstr (v1, l) when v = v1 -> Labstr (v1, l)
-  | Labstr (v1, l) -> Labstr(v1, my_subst l)
-  | Lapp(l1, l2) -> Lapp (my_subst l1, my_subst l2)
-  | Llet (v1, l1, l2) when v = v1 -> Llet (v1, l1, l2)
-  | Llet (v1, l1, l2) -> Llet (v1, my_subst l1, my_subst l2)
-  | Lconst c -> Lconst c
-  | Lunop(op, l1) -> Lunop(op, my_subst l1)
-  | Lbinop(op, l1, l2) -> Lbinop (op, my_subst l1, my_subst l2)
-  | Ltuple l1 -> Ltuple (List.map my_subst l1)
-  | Lfield(l, n) -> Lfield(my_subst l, n)  (*could be optimised ?*)
-  | Lcoerce _ -> assert false (*subst should only be applied to expanded Lambda expressions*)
-  
+
 let rec apply_coercion (c: coercion) (l: lam) : lam = 
   match c, l with 
   | Cid, _ -> l
   | Cint2float, _ -> Lunop(Ofloatofint, l)
-  | Cfun(c1, c2), Labstr(v, l1) -> apply_coercion c2 (Labstr(v, (subst v (apply_coercion c1 (Lvar v)) l1)))
-  | Crecord l1, Ltuple l2 -> 
+  | Cfun(c1, c2), _ ->
+	let x = fresh_variable () in
+	Labstr(x, apply_coercion c2 (Lapp (l, apply_coercion c1 (Lvar x))))
+ (* | Crecord l1, Ltuple l2 -> 
     begin match l1 with 
 	| [] -> Ltuple []
 	| (i,c)::t ->  append_tuple (apply_coercion c (elt i l2)) (apply_coercion (Crecord t) l)
-	end
-  | _-> assert false
+	end*)
+  | Crecord l1, _ ->
+	let x = fresh_variable () in
+	Llet(x, l, Ltuple (List.map (fun (i,c) -> apply_coercion c (Lfield((Lvar x),i)))  l1))
 (* Recursively expand the coercions by applying them. *)
 
 let rec expand (l: lam): lam =
--- src/Optimize.ml
+++ src/Optimize.ml
@@ -5,4 +5,7 @@ open IL
 
 (* Optimization of expressions *)
 
-let rec optim (l: lam) : lam = l 
+let rec optim (l: lam) : lam = 
+	match l with
+	| Lcoerce(l1, Crecord l2) when List.for_all (function (_, Cid) -> true |_-> false) l2 -> l1
+	| _ -> l
