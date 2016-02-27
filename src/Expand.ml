0cbea64c291cad583635502febcbca179fe65e18
raphaelrieuhelft <raphael.rieuhelft@gmail.com>
2016-02-28 00:55:42 +0100

    Add some optimisations

--- src/Expand.ml
+++ src/Expand.ml
@@ -8,26 +8,16 @@ open IL
 (* Apply a coercion [c] to the expression [l].  The result is a 
    coercion-free Lambda expression. *)
 
-let rec elt i = function
-  | [] -> assert false (*this should never happen, too short tuples are detected at typing*)
-  | h::t when i = 1 -> h
-  |_ ::t -> elt (i-1) t
-
 let append_tuple (l: lam) = function Ltuple l2 -> Ltuple (l::l2) |_-> assert false 
 
 let rec apply_coercion (c: coercion) (l: lam) : lam = 
-  match c, l with 
-  | Cid, _ -> l
-  | Cint2float, _ -> Lunop(Ofloatofint, l)
-  | Cfun(c1, c2), _ ->
+  match c with 
+  | Cid -> l
+  | Cint2float -> Lunop(Ofloatofint, l)
+  | Cfun(c1, c2) ->
 	let x = fresh_variable () in
 	Labstr(x, apply_coercion c2 (Lapp (l, apply_coercion c1 (Lvar x))))
- (* | Crecord l1, Ltuple l2 -> 
-    begin match l1 with 
-	| [] -> Ltuple []
-	| (i,c)::t ->  append_tuple (apply_coercion c (elt i l2)) (apply_coercion (Crecord t) l)
-	end*)
-  | Crecord l1, _ ->
+  | Crecord l1 ->
 	let x = fresh_variable () in
 	Llet(x, l, Ltuple (List.map (fun (i,c) -> apply_coercion c (Lfield((Lvar x),i)))  l1))
 (* Recursively expand the coercions by applying them. *)
--- src/Optimize.ml
+++ src/Optimize.ml
@@ -1,5 +1,5 @@
 (* Optimization of coercions in an IL expression *)
-
+open AST
 open IL
 
 
@@ -18,9 +18,16 @@ let rec compose_coercions c1 c2 = match c1, c2 with
 			
 
 
+let rec is_id_record l i = match l with
+	| [] -> true
+	| (i,Cid)::t -> is_id_record t (i+1)
+	| _ -> false
+			
 let rec optim_coercion (l: lam) (c:coercion) : lam = 
 	match l,c with
-	| Ltuple l1, Crecord l2 when List.length l1 = 1 && l2 = [(1, Cid)] -> Lcoerce(Ltuple l1, Cid)
+	| Ltuple l1, Crecord l2 when List.length l1 = List.length l2 && (is_id_record l2 1) 
+		-> Lcoerce(Ltuple l1, Cid)
+	| _, Cfun(Cid, Cid) -> Lcoerce(l, Cid)
 	| Ltuple l1, Crecord l2 -> 
 		let small_tuple = List.map (fun (i,c) -> List.nth l1 (i-1)) l2 in
 		let rec renumber i = function 
@@ -29,17 +36,24 @@ let rec optim_coercion (l: lam) (c:coercion) : lam =
 		in
 		Lcoerce(Ltuple small_tuple, Crecord (renumber 1 l2))
 	| Lcoerce(l1, c1), c2 -> optim (Lcoerce(l1, compose_coercions c2 c1))	
+	| Labstr(v,a), Cfun(c1, c2) -> 
+		let x = fresh_variable () in
+		optim (Labstr(x, Lcoerce(Llet(v, Lcoerce(Lvar x, c1), a), c2)))
 	| _ -> Lcoerce(l,c)
 
 and optim (l: lam): lam =
-  match l with
-  | Lvar v -> Lvar v
-  | Labstr(v, l) -> Labstr(v, optim l)
-  | Lapp(l1, l2) -> Lapp(optim l1, optim l2)
-  | Llet(v, l1, l2) -> Llet(v, optim l1, optim l2)
-  | Lconst c -> Lconst c
-  | Lunop(op, l1) -> Lunop(op, optim l1)
-  | Lbinop(op, l1, l2) -> Lbinop(op, optim l1, optim l2)
-  | Ltuple ll -> Ltuple (List.map optim ll)
-  | Lfield(l, n) -> Lfield(optim l, n)
-  | Lcoerce(l,c) -> optim_coercion l c
+	match l with
+	| Lvar v -> Lvar v
+	| Labstr(v, l) -> Labstr(v, optim l)
+	| Lapp(Lcoerce(f, Cfun(c1, c2)), arg) -> optim (Lcoerce(Lapp(f, Lcoerce(arg, c1)),  c2))
+	| Lapp(l1, l2) -> Lapp(optim l1, optim l2)
+	| Llet(v, l1, l2) -> Llet(v, optim l1, optim l2)
+	| Lconst c -> Lconst c
+	| Lunop(op, l1) -> Lunop(op, optim l1)
+	| Lbinop(op, l1, l2) -> Lbinop(op, optim l1, optim l2)
+	| Ltuple ll -> Ltuple (List.map optim ll)
+	| Lfield(Lcoerce(l1, Crecord lc), n) ->
+		let (i, c) = List.nth lc (n-1) in
+		optim (Lcoerce(Lfield(l1, i), c))
+	| Lfield(l, n) -> Lfield(optim l, n)
+	| Lcoerce(l,c) -> optim_coercion (optim l) c
